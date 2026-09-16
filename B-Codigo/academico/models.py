0a533e70abac87ff44983fac5e6f197c1b4385bc
SantillanDottaLucas <lucassantillanrg@gmail.com>
2026-09-16 15:08:46 -0300

    XD, de todo

--- B-Codigo/academico/models.py
+++ B-Codigo/academico/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import tree
 
 # Create your models here.
+
+class Graduado(models.Model):
+    legajoGraduado = models.CharField(max_length=14, primary_key=True)
+    dniGraduado = models.CharField(max_length=9, unique=True)
+    fechaNacimiento= models.DateField()
+    
